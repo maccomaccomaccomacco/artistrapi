d4f4c92a0a9383a628ab9b846d0426831928e9a1
marcoautiero <marcoautiero@gmail.com>
2023-12-21 21:16:58 +0100

    Init, I think

---
+++ config/plugins.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // ...
+    'artrenewal-scraper': {
+      enabled: true,
+      resolve: './src/plugins/artrenewal-scraper' // path to plugin folder
+    },
+    // ...
+  }
---
+++ src/plugins/artrenewal-scraper/README.md
@@ -0,0 +1,3 @@
+# Strapi plugin artrenewal-scraper
+
+A quick description of artrenewal-scraper.
